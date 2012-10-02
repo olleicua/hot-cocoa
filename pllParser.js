bf79c95af1f11e0cf0d21f6047f2ae1c5a955d39
Sam Auciello <olleicua@gmail.com>
2012-10-02 01:29:18 -0400

    Improved parsing of PLL

--- pllParser.js
+++ pllParser.js
@@ -15,44 +15,49 @@ var tokenTypes = [
     { t:'prefix-operator', re:/^not\b/ },
     { t:'literal', re:/^(true|false)\b/ },
     { t:'variable', re:/^[a-zA-Z_][a-zA-Z0-9_]*/ },
-    { t:'(', re:/^\("/ },
+    { t:'(', re:/^\(/ },
     { t:')', re:/^\)/ },
     { t:'=', re:/^=/ },
     { t:'newline', re:/^\n/ },
-    { t:'whitespace', re:/^\s*/ }
+    { t:'whitespace', re:/^\s+/ }
 ];
 
 var grammar = {
     '_program': [
-        ['_statement'],
-		['_statement', '_program']
+        ['_statement', '_program-tail'],
     ],
+	'_program-tail': [
+		['_program'],
+		[]
+	],
     '_statement': [
-        ['_assignment', 'newline'],
-        ['_expression', 'newline']
+        ['_assignment'],
+        ['_expression']
     ],
     '_assignment': [
         ['variable', '=', '_expression']
     ],
     '_expression': [
-        ['_unambiguous-expression'],
-        ['_unambiguous-expression', 'infix-operator', '_unambiguous-expression'],
+        ['_unambiguous-expression', 'newline'],
+        ['_unambiguous-expression', 'infix-operator', '_unambiguous-expression', 'newline']
     ],
 	'_unambiguous-expression': [
 		['literal'],
-		['(', '_expression', ')']
+		['(', '_expression', ')'],
 		['prefix-operator', '_unambiguous-expression']
 	]
 };
 
 var tests = [
-    'true',
-    'true and false',
+    'true\n',
+    'true and false\n',
     'p = true\nq = false\n(p and q) implies (p or q)'
 ];
 
 for (var i = 0; i < tests.length; i++) {
     var tokens = scanner.scan(tokenTypes, tests[i]);
+    console.log('---');
+    console.log(JSON.stringify(tokens));
     var tree = parser.parse(tokens, grammar, "_program");
     console.log(JSON.stringify(tree));
 }
