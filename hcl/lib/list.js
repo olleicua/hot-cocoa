08162ef7217543ff3761d9f337fcef5ef99326eb
Sam Auciello <olleicua@gmail.com>
2012-10-17 14:54:16 -0400

    Making list into wrapper objects to conform better to structure of the other types

--- hcl/lib/list.js
+++ hcl/lib/list.js
@@ -12,50 +12,55 @@ _String.toString = function() { return this.text; };
 _List.zero_index = 0;
 _List.get = function(index) {
 	if (index.type === 'number') {
-		return this[this.zero_index + index.value];
-	}
+		return this.values[this.zero_index + index.value];
+	} // should return undefined be explicit?
 }
 _List.set = function(index, value) {
 	if (index.type === 'number') {
-		return this[this.zero_index + index.value] = value;
+		return this.values[this.zero_index + index.value] = value;
 	}
 }
 _List.first = function() {
-	return this[this.zero_index];
+	return this.values[this.zero_index];
 }
 _List.rest = function() {
-	// This is wrong, we want pointers. _List should be a real wrapper..
-	var result = Object.create(this);
+	var result = Object.create(_List);
 	result.zero_index = this.zero_index + 1;
+	result.values = this.values;
 	return result;
+}
+_List.size = function() {
+	return this.values.length - this.zero_index;
 }
 _List.toArray = function() { // is this necessary??
 	var result = [];
-	for (var i = this.zero_index; i < this.length; i++) {
-		result.push(this[i]);
+	for (var i = this.zero_index; i < this.values.length; i++) {
+		result.push(this.values[i]);
 	}
 	return result;
 }
 _List.map = function(func) {
 	var result = Object.create(_List);
-	for (var i = this.zero_index; i < this.length; i++) {
-		result.push(func(this[i]));
+	result.values = [];
+	for (var i = this.zero_index; i < this.values.length; i++) {
+		result.values.push(func(this.values[i]));
 	}
 	return result;
 }
 _List.filter = function(func) {
 	var result = Object.create(_List);
-	for (var i = this.zero_index; i < this.length; i++) {
-		if (func(this[i])) {
-			result.push(this[i]);
+	result.values = [];
+	for (var i = this.zero_index; i < this.values.length; i++) {
+		if (func(this.values[i])) {
+			result.values.push(this.values[i]);
 		}
 	}
 	return result;
 }
 _List.reduce = function(func, init) {
 	var result = init;
-	for (var i = this.zero_index; i < this.length; i++) {
-		result = func(result, this[i]);
+	for (var i = this.zero_index; i < this.values.length; i++) {
+		result = func(result, this.values[i]);
 	}
 	return result;
 }
@@ -65,8 +70,9 @@ _List.copy = function() {
 
 exports.new_list = function() {
 	var result = Object.create(_List);
+	result.values = [];
 	for (var i = 0; i < arguments.length; i++) {
-		result.push(arguments[i]);
+		result.values.push(arguments[i]);
 	}
 	return result;
 }
