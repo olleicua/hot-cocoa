c2905779484a9c7a99a9776497031527b4ec699b
Jim Mahoney <mahoney@marlboro.edu>
2012-10-23 15:23:49 -0400

    Jim added a comment

--- parser.test.js
+++ parser.test.js
@@ -4,6 +4,7 @@
 var parser = require('./parser.js');
 
 var tests = [
+
 	[function() {
 		var tokens = [{"type":"a"}, {"type":"b"}];
 		var grammar = {
@@ -15,6 +16,11 @@ var tests = [
 		return JSON.stringify(tree);
 	},
 	 '[{"type":"_ab","tree":[{"type":"a"},{"type":"b"}]}]'],
+
+
+    \\ This one fails when the parser goes down the "x is [1 2 3]"
+    \\ path, but then when it fails to apply the _a rule,
+    \\ it doesn't back out properly to try _x as [1 2]. - Jim
 	[function() {
 		var tokens = [{"type":"1"}, {"type":"2"}, {"type":"3"}];
 		var grammar = {
