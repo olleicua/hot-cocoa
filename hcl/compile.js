42c437100a8689f3fd42b671644f80dccd01bcce
Sam Auciello <olleicua@gmail.com>
2012-12-17 13:24:49 -0500

    Updated compiler to pass tests

--- hcl/compile.js
+++ hcl/compile.js
@@ -1,5 +1,6 @@
 var template = require('../tools/template.js');
 var hcl = require('./hclParser.js');
+var types = require('./lib/types.js');
 
 var string2ast = function(string) {
     var tokens = hcl.scan(string);
@@ -7,37 +8,30 @@ var string2ast = function(string) {
     return hcl.analyze(tree);
 };
 
-// TODO: fix the infrastructure
-// all types need a 'stringify' method as well as a bare method
-// perhaps no infrastructure is needed at all...?
-// I think I want to leave everything literal except for strings and words which
-// will be objects with a relevant .toJSON method implemented:
-// https://developer.mozilla.org/en-US/docs/JSON
-// possibly also overriding all of the .toString methods..?
-
 var macros = {};
+
+var apply_macros = function(ast) {
+	// TODO: implement this
+	return ast;
+}
 
 var quaziquote = function(ast) {
 
     if (ast.type === 'list') {
         
         // unquote
-        if (ast.first().bare() === 'unquote') {
-            return compile(ast.get(1)).bare();
+        if (ast[0].json() === 'unquote') {
+            return compile(ast[0]).json();
         }
         
         // quoted list
         return ast.map(function(value) {
             return quaziquote(value);
-        });
+        }).json();
     }
 
-    // TODO: add macro-exists here
-
-    // TODO: add compile error here
-    
     // atom
-    return ast.bare();
+    return ast.json();
 }
 
 var compile = function(ast) {
@@ -46,39 +40,44 @@ var compile = function(ast) {
     if (ast.type === 'list') {
         
         // formatted javascript
-        if (ast.first().bare() === 'quote') {
-            // TODO: quoted words need to be represented somehow..?
-            return ast.get(1).bare();
+        if (ast[0].json() === 'quote') {
+            return ast[1].json();
         }
         
         // formatted javascript
-        if (ast.first().bare() === 'quaziquote') {
-            return quaziquote(ast.get(1));
+        if (ast[0].json() === 'quaziquote') {
+            return quaziquote(ast[1]);
         }
         
         // formatted javascript
-        if (ast.first().bare() === 'js') {
+        if (ast[0].json() === 'js') {
             return template.format(
-                ast.get(1),
-                ast.rest().rest().map(function(value) {
+                ast[1].json(),
+                types.list(ast.slice(2)).map(function(value) {
                     return compile(value);
                 })
             );
         }
         
+        // implement macro-exists
+        if (ast[0].json() === 'macro-exists') {
+            return macros[ast[1].json()] === undefined ? 'false' : 'true';
+        }
+        
+        // TODO: add compile error handling here
+        
         // formatted hcl
         return template.format('~~(~~)', [
-            compile(ast.first()),
-            ast.rest().map(function(value) {
-                //console.log(value, value.bare(), compile(value), value.type);
+            compile(ast[0]),
+            types.list(ast.slice(1)).map(function(value) {
                 return compile(value);
-            }).bare().join(', ')
+            }).join(', ')
         ]);
     }
     
     // scalar expression
-    //console.log(ast, ast.bare());
-    return ast.bare();
+    return ast.json();
+    
     // TODO: add more tests
 };
 
@@ -88,19 +87,17 @@ exports.compile = function(text) {
     var asts = string2ast(text);
     
     for (var i = 0; i < asts.length; i++) {
-        var prepared_ast = asts[i];
-        // TODO: implement this
-        // var prepared_ast = apply_macros(asts[i]);
+        var prepared_ast = apply_macros(asts[i]);
         // FIXME: macros should recursively apply to themselves..
-        if (prepared_ast.first().bare() === 'macro') {
-            var name = macros[prepared_ast.get(1).bare()];
+        // I think this means a first pass to store macros
+        if (prepared_ast[0].json() === 'macro') {
+            var name = macros[prepared_ast[1].json()];
             if (name) {
                 throw new Error('There is already a macro called ' + name);
                 // TODO: add line number
             }
             macros[name] = prepared_ast;
         } else {
-            //console.log(JSON.stringify(compile(prepared_ast)), JSON.stringify(prepared_ast));
             compiled_statements.push(compile(prepared_ast) + ';');
         }
     }
--- hcl/tests/hello.js.hcl
+++ hcl/tests/hello.js.hcl
@@ -1,3 +1,1 @@
 alert("Hello World!");
-
-alert("hello!");
