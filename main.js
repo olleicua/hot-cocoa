a9b3fae84883a288a9e02b367db86c22485326c2
Sam Auciello <olleicua@gmail.com>
2013-01-10 15:00:55 -0500

    Added main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+// scanner
+exports.scan = require('./lib/scanner.js').scan;
+
+// parser
+exports.RD = 1;
+exports.CYK = 2;
+exports.parse = function(tokens, grammar, start_node, algorithm) {
+	if (algorithm === this.RD) {  // use recursive descent
+		return require('./lib/RDparser.js').parse(tokens, grammar, start_node);
+	} // use CYK
+	return require('./lib/CYKparser.js').parse(tokens, grammar, start_node);
+}
+
+// analyzer
+exports.analyzer = require('./lib/analyzer.js').analyzer;
+
+// template
+exports.format = require('./lib/template.js').format;
+exports.formatFile = require('./lib/template.js').load;
+
+// test
+exports.test = require('./lib/test.js').test;
